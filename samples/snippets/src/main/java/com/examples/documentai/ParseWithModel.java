9a3e85b31bc69123c26aed87bec974dc1af9b15e
Mike Ganbold <munkhbayarg@google.com>
2020-04-10 14:00:55 -0700

    reformatted automl param

--- samples/snippets/src/main/java/com/examples/documentai/ParseWithModel.java
+++ samples/snippets/src/main/java/com/examples/documentai/ParseWithModel.java
@@ -45,7 +45,9 @@ public class ParseWithModel {
       // Configure the request for processing the PDF
       String parent = String.format("projects/%s/locations/us", projectId);
 
-      AutoMlParams params = AutoMlParams.newBuilder().setModel(autoMlModelId).build();
+      String model =
+          String.format("projects/%s/locations/us-central1/models/%s", projectId, autoMlModelId);
+      AutoMlParams params = AutoMlParams.newBuilder().setModel(model).build();
 
       GcsSource uri = GcsSource.newBuilder().setUri(gcsUri).build();
 
